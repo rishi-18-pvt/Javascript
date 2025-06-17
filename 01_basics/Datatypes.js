1da270b0612e4522e2c5ed6ec70930b04585d6d2
Rishi R <rishirreddy18@gmail.com>
2025-06-17 08:11:52 +0000

    datatypes js

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,15 @@
+let name = "Rishi"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//number => range 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null
+// undefined
+// symbol
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
