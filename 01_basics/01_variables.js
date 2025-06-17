c46ec56c387d17648a3f268ae38ffc11dcfb5a5d
Rishi R <rishirreddy18@gmail.com>
2025-06-17 05:08:42 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 1888
+var accountEmail = "rishi@gmail.com"
+let accountPassword = 1234
+accountCity = "coimbatore"
+
+
+accountEmail = "rishireddy18@gmail.com"
+accountPassword = 1234
+accountcity = "kalyan"
+console.table([accountID, accountEmail, accountPassword,accountCity])
+
+/*
+prefer not to use var
+beacause of issue in block space
+*/
