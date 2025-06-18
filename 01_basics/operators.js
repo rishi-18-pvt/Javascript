c27378c3aae736b185155444916f09d6a168a5d1
Rishi R <rishirreddy18@gmail.com>
2025-06-18 14:54:51 +0000

    operators

---
+++ 01_basics/operators.js
@@ -0,0 +1,28 @@
+let value = 8
+let negValue = -value
+//console.log(negValue); => -8
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); => 2 to the power 3
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Rishi"
+let str3 = str1 + str2
+//console.log(str3); => hello Rishi
+
+//console.log(1 + "2"); => 12
+//console.log("1" + 2); => 12
+//console.log(1 + 2 + "8"); => 38  //3 + "8" → now one operand is a string → JavaScript converts 3 to "3" and performs string
+//console.log("1"+2+8); => 128 */
+// "1" + 2 → "1" is a string → 2 is converted to "2" → "1" + "2" = "12" Then: "12" + 8 → 8 is converted to "8" → "12" + "8" = "128"/*
+
+let gameCounter = 433
+++gameCounter
+//console.log(gameCounter); => 434
+
+
+
