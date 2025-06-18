a72042913ace7ff0927ffb802d86ecc7a6a7d55e
Rishi R <rishirreddy18@gmail.com>
2025-06-18 05:06:25 +0000

    datatype conversion

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,24 @@
+let score = "33"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let IsloggedIn = ""
+let booleanIsloggedIn = Boolean(IsloggedIn)
+console.log(booleanIsloggedIn);
+
+//" " => false
+//"Name" => true
+// 1 => true ; 0 => false
+
+let SomeNumber = 88
+let StringNumber = String(SomeNumber)
+console.log(StringNumber);
+
+
